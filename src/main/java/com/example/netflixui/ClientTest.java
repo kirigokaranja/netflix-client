97a0ef6bb0975c636627db7d59704e233e90551c
sharon kirigo <sharonkirigo@gmail.com>
2019-10-30 12:27:15 +0300

    fetch movie by category type & update and delete movie

--- src/main/java/com/example/netflixui/ClientTest.java
+++ src/main/java/com/example/netflixui/ClientTest.java
@@ -1,9 +1,6 @@
 package com.example.netflixui;
 
-import com.example.netflixui.models.Category;
-import com.example.netflixui.models.Movie;
-import com.example.netflixui.models.User;
-import com.example.netflixui.models.UserType;
+import com.example.netflixui.models.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -23,16 +20,15 @@ public class ClientTest implements CommandLineRunner {
         //create a user
         User newUser = new User("Sharon", 345689, UserType.user);
         newUser = feignRestClient.createUser(newUser);
-        System.out.println("New user" + newUser);
+        System.out.println("New user: " + newUser);
 
         //get all users
         List<User> users = feignRestClient.getAllUsers();
-        System.out.println("All users" + users);
+        System.out.println("All users: " + users);
 
         //get one user
-//        User myUser = feignRestClient.findById((long) 3);
-//        User myUser = feignRestClient.findById(newUser.getId());
-//        System.out.println("My User is: " + myUser);
+        User myUser = feignRestClient.findById(newUser.getId());
+        System.out.println("My User is: " + myUser);
 
         //update a user
 //        User updatedUser = new User("Kirigo", 345689);
@@ -45,17 +41,17 @@ public class ClientTest implements CommandLineRunner {
 //        System.out.println("Deleted User" + deletedUser);
 
         //create a category
-//        Category newcategory = new Category("Horror");
-//        newcategory = feignRestClient.createCategory(newcategory);
-//        System.out.println("New Category" + newcategory);
+        Category newcategory = new Category("Horror");
+        newcategory = feignRestClient.createCategory(newcategory);
+        System.out.println("New Category: " + newcategory);
 
         //get all categories
         List<Category> categories = feignRestClient.getAllCategories();
-        System.out.println("All categories" + categories);
+        System.out.println("All categories: " + categories);
 
         //get one category
-//        Category myCategory = feignRestClient.findCategoryById(newcategory.getId());
-//        System.out.println("Updated Category: " + myCategory);
+        Category myCategory = feignRestClient.findCategoryById(newcategory.getId());
+        System.out.println("One Category: " + myCategory);
 
         //update a category
 //        Category updatedCategory = new Category("Drama");
@@ -68,14 +64,37 @@ public class ClientTest implements CommandLineRunner {
 
         //create a movie
         Category category1 = feignRestClient.findCategoryById((long)1);
-        Category category2 = feignRestClient.findCategoryById((long)2);
+        Category category2 = feignRestClient.findCategoryById((long)6);
         List<Category> findCategories = new ArrayList<Category>();
         findCategories.add(category1);
         findCategories.add(category2);
-        Movie newMovie = new Movie("The wedding Year", 2019, findCategories);
-        newMovie = feignRestClient.createMovie(newUser.getId(), newMovie);
+        Movie newMovie = new Movie("The Rambo", 1990, findCategories);
+        newMovie = feignRestClient.createMovie((long)3, newMovie);
         System.out.println("New Movie: " + newMovie);
 
         //get movies by category id and type
+        List<Movie> findMovie = feignRestClient.movieByCategoryAndType((long)1, MovieType.suggested);
+        System.out.println("Movie by Category and Type: " + findMovie);
+
+        //get all movies
+        List<Movie> movies = feignRestClient.allMovies();
+        System.out.println("All movies: " + movies);
+
+        //get one movie
+        Movie oneMovie = feignRestClient.findMovieById(newMovie.getId());
+        System.out.println("One Movie Found: " + oneMovie);
+
+        //update a movie
+//        Category category3 = feignRestClient.findCategoryById((long)6);
+//        findCategories.add(category1);
+//        findCategories.add(category2);
+//        findCategories.add(category3);
+//        Movie updateMovie = new Movie("The wedding Year 2", 2019, findCategories);
+//        updateMovie = feignRestClient.updateMovie((long)7,(long)5,updateMovie);
+//        System.out.println("Updated Movie: " + updateMovie);
+
+        //delete a movie
+//        String deleteMovie = feignRestClient.deleteMovie((long)8,(long)5);
+//        System.out.println("Movie " + deleteMovie + " deleted successfully");
     }
 }
--- src/main/java/com/example/netflixui/FeignRestClient.java
+++ src/main/java/com/example/netflixui/FeignRestClient.java
@@ -2,16 +2,14 @@ package com.example.netflixui;
 
 import com.example.netflixui.models.Category;
 import com.example.netflixui.models.Movie;
+import com.example.netflixui.models.MovieType;
 import com.example.netflixui.models.User;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "client",url = "http://10.52.26.35:8080/", configuration = FeignConfig.class)
+@FeignClient(name = "client",url = "https://netflix-api.herokuapp.com/", configuration = FeignConfig.class)
 public interface FeignRestClient {
     //Create a user
     @RequestMapping(method = RequestMethod.POST, value = "users")
@@ -58,4 +56,22 @@ public interface FeignRestClient {
     Movie createMovie(@PathVariable(name = "id") Long id, @RequestBody Movie movie);
 
     //get movies by category id and type
+    @RequestMapping(method = RequestMethod.GET, value = "movies/category/{id}")
+    List<Movie> movieByCategoryAndType(@PathVariable(name = "id") Long id, @RequestParam(value = "type") MovieType type);
+
+    //get all movies
+    @RequestMapping(method = RequestMethod.GET, value = "movies")
+    List<Movie> allMovies();
+
+    //get one mvoie
+    @RequestMapping(method = RequestMethod.GET, value = "movies/{id}")
+    Movie findMovieById(@PathVariable(name = "id")Long id);
+
+    //update one movie
+    @RequestMapping(method = RequestMethod.PATCH, value = "users/{userid}/movie/{id}")
+    Movie updateMovie(@PathVariable(value = "id") Long id, @PathVariable(value = "userid") Long userid, @RequestBody Movie movie);
+
+    //delete movie
+    @RequestMapping(method = RequestMethod.DELETE, value = "users/{userid}/movie/{id}")
+    String deleteMovie(@PathVariable(value = "id") Long id, @PathVariable(value = "userid") Long userid);
 }
